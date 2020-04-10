b4858b87e7d6fb507d631017c055387545f9627a
nickmachiavelli7 <41245311+nickmachiavelli7@users.noreply.github.com>
2020-04-10 12:20:20 -0500

    Update roll.js
    Actually building my own code this time

--- commands/roll.js
+++ commands/roll.js
@@ -3,43 +3,11 @@ module.exports = {
 	description: 'Rolls the dice. Format: "roll NdS" where N is the number of dice and S is the size of those dice. You can also just call the command with a single number to roll one die of that size.',
 	cooldown: 5,
 	execute(message, args) {
-		const messageWords = message.content.split(' ');
-		const rollFlavor = messageWords.slice(2).join(' ');
-			if (messageWords[0] === '!roll') {
-				if (messageWords.length === 1) {
-					// !roll
-				return message.reply(
-					(Math.floor(Math.random() * 6) + 1) + ' ' + rollFlavor
-					);
-    }
-
-    let sides = messageWords[1]; // !roll 20
-    let rolls = 1;
-    if (!isNaN(messageWords[1][0] / 1) && messageWords[1].includes('d')) {
-      // !roll 4d20
-      rolls = messageWords[1].split('d')[0] / 1;
-      sides = messageWords[1].split('d')[1];
-    } else if (messageWords[1][0] == 'd') {
-      // !roll d20
-      sides = sides.slice(1);
-    }
-    sides = sides / 1; // convert to number
-    if (isNaN(sides) || isNaN(rolls)) {
-      return;
-    }
-    if (rolls > 1) {
-      const rollResults = [];
-      for (let i = 0; i < rolls; i++) {
-        rollResults.push(Math.floor(Math.random()*sides)+1);
-      }
-      const sum = rollResults.reduce((a,b) => a + b);
-      return message.reply(`[${rollResults.toString()}] ${rollFlavor}`);
-    } else {
-      return message.reply(
-        (Math.floor(Math.random() * sides) + 1) + ' ' + rollFlavor
-      );
-    }
-  }
-		message.channel.send('Pong.');
+		if args.includes('d'){
+			rolls = args[1].split('d')[0] / 1;
+			sides = args[1].split('d')[1];
+			
+		}
+		message.channel.send('You asked for '+ rolls +' rolls of a '+ sides +'sided die.');
 	},
 };
