a15d50d1d89cb2e6ffedca272b85c51e06e6f75b
nickmachiavelli7 <41245311+nickmachiavelli7@users.noreply.github.com>
2020-04-10 13:59:43 -0500

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -33,4 +33,6 @@ module.exports = {
 		else
 			message.channel.send('Something you did was bad. Try not being bad.')
 	},
+			else
+			message.channel.send('Something you did was bad. Try not being bad.')
 };
