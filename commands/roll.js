13b5ecfbf99ce507deca685da5ac1dd0ef5b6178
nickmachiavelli7 <41245311+nickmachiavelli7@users.noreply.github.com>
2020-04-10 13:10:50 -0500

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -1,6 +1,7 @@
 module.exports = {
 	name: 'roll',
-	description: 'Rolls the dice. Format: "roll NdS" where N is the number of dice and S is the size of those dice. You can also just call the command with a single number to roll one die of that size.',
+	description: 'Rolls the dice. Format: "roll NdS (+/-M)" where N is the number of dice and S is the size of those dice. M is an optional modifier to the roll. A space must be included before the modifier. You can also just call the command with a single number to roll one die of that size.',
+	args: true,
 	cooldown: 5,
 	execute(message, args) {
 		const request = args[0];
@@ -12,8 +13,18 @@ module.exports = {
 			for(i=0;i<rolls;i++){
 				sum = (Math.floor(Math.random() * (sides - 1 + 1) ) + 1) + sum;
 			}
+			sum= sum + args[1];
 			message.channel.send('You rolled '+ rolls +' '+ sides +'-sided dice and got a total of '+ sum +'.');
 			
 		}
+		else if(!isNaN(request.includes('d')))
+			sides = args[0] / 1;
+		roll = (Math.floor(Math.random() * (sides - 1 + 1) ) + 1);
+			if(roll == 1){
+				message.channel.send('CRITICAL FAILURE - you rolled a nat 1!');
+			}
+			else if(roll == 20){
+				message.channel.send('CRITICAL SUCCESS! - you rolled a nat 20!');
+			}
 	},
 };
