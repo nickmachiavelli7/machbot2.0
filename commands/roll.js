1397b9462cb9d6961e991df3d2374c4610dd7b3e
nickmachiavelli7 <41245311+nickmachiavelli7@users.noreply.github.com>
2020-04-10 12:26:02 -0500

    Update roll.js
    Trouble is shot

--- commands/roll.js
+++ commands/roll.js
@@ -3,9 +3,9 @@ module.exports = {
 	description: 'Rolls the dice. Format: "roll NdS" where N is the number of dice and S is the size of those dice. You can also just call the command with a single number to roll one die of that size.',
 	cooldown: 5,
 	execute(message, args) {
-		if args.includes('d'){
-			rolls = args[1].split('d')[0] / 1;
-			sides = args[1].split('d')[1];
+		if args[0].includes('d'){
+			rolls = args[0].split('d')[0] / 1;
+			sides = args[0].split('d')[1];
 			
 		}
 		message.channel.send('You asked for '+ rolls +' rolls of a '+ sides +'sided die.');
