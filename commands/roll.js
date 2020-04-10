9270337fd0a798a19c25b588601bc513ee55f113
nickmachiavelli7 <41245311+nickmachiavelli7@users.noreply.github.com>
2020-04-10 12:32:12 -0500

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'Rolls the dice. Format: "roll NdS" where N is the number of dice and S is the size of those dice. You can also just call the command with a single number to roll one die of that size.',
 	cooldown: 5,
 	execute(message, args) {
-		request = args[0];
+		const request = args[0];
 		if request.includes('d'){
 			rolls = args[0].split('d')[0] / 1;
 			sides = args[0].split('d')[1];
