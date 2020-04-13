ccef16727cc1bf2a3ec469e8233f57dbb698ffaa
nickmachiavelli7 <41245311+nickmachiavelli7@users.noreply.github.com>
2020-04-13 12:52:52 -0500

    Revert "Update roll.js"
    This reverts commit 25541b81debcd0045dd3976c9613053804308455.

--- commands/roll.js
+++ commands/roll.js
@@ -1,17 +1,32 @@
 module.exports = {
 	name: 'roll',
-	description: 'Rolls the dice. Format: "roll NdS (+/-M) A/D" where N is the number of dice and S is the size of those dice. M is an optional modifier to the roll. A space must be included before the modifier. You can also just call the command with a single number to roll one die of that size. Adding "A" to the end indicates advantage (higher of two values) and "D" indicates disadvantage (lower of two values).',
+	description: 'Rolls the dice. Format: "roll NdS (+/-M)" where N is the number of dice and S is the size of those dice. M is an optional modifier to the roll. A space must be included before the modifier. You can also just call the command with a single number to roll one die of that size.',
 	args: true,
 	cooldown: 5,
 	execute(message, args) {
+<<<<<<< HEAD
+<<<<<<< HEAD
 		let roller = function (rolls, sides, mod) {
 			var sum = 0;
 			var i = 0;
+=======
+=======
+>>>>>>> parent of d8345e2... Adding (dis)advantage capability, and more.
+		const request = args[0];
+		if (request.includes('d')){
+			rolls = args[0].split('d')[0] / 1;
+			sides = args[0].split('d')[1] / 1;
+			mod = (args[1] / 1);
+			let sum = 0;
+			let i = 0;
+>>>>>>> parent of d8345e2... Adding (dis)advantage capability, and more.
 			for(i=0;i<rolls;i++){
 				sum = (Math.floor(Math.random() * (sides - 1 + 1) ) + 1) + sum;
 			}
 			if (!isNaN(mod))
 				sum= sum + mod;
+<<<<<<< HEAD
+<<<<<<< HEAD
 			return sum;
 			};
 		const request = args[0];
@@ -82,20 +97,36 @@ module.exports = {
 				sides = args[0].split('d')[1] / 1;
 				num1 = roller(rolls,sides, mod);
 				message.channel.send('You rolled '+ rolls +' '+ sides +'-sided dice and got a total of '+ num1 +'.');
+=======
+			message.channel.send('You rolled '+ rolls +' '+ sides +'-sided dice and got a total of '+ sum +'.');
+			
+		}
+		else if(!isNaN(request.includes('d'))){
+			sides = args[0] / 1;
+		roll = (Math.floor(Math.random() * (sides - 1 + 1) ) + 1);
+			if(roll == 1 && sides = 20){
+				message.channel.send('CRITICAL FAILURE - you rolled a nat 1!');
 			}
-			else if(!isNaN(request.includes('d')))
-			{
-				sides = args[0] / 1;
-				roll = roller(1,sides,mod);
-				if((roll == 1) && (sides == 20)){
-					message.channel.send('CRITICAL FAILURE - you rolled a nat 1!');
-				}
-				else if((roll == 20) && (sides ==20)){
-					message.channel.send('CRITICAL SUCCESS! - you rolled a nat 20!');
-				}
-				else{
-					message.channel.send('You rolled a ' + roll + '.');
-				}
+			else if(roll == 20 && sides = 20){
+				message.channel.send('CRITICAL SUCCESS! - you rolled a nat 20!');
+>>>>>>> parent of d8345e2... Adding (dis)advantage capability, and more.
+			}
+=======
+			message.channel.send('You rolled '+ rolls +' '+ sides +'-sided dice and got a total of '+ sum +'.');
+			
+		}
+		else if(!isNaN(request.includes('d'))){
+			sides = args[0] / 1;
+		roll = (Math.floor(Math.random() * (sides - 1 + 1) ) + 1);
+			if((roll == 1) && (sides == 20)){
+				message.channel.send('CRITICAL FAILURE - you rolled a nat 1!');
+			}
+			else if((roll == 20) && (sides ==20)){
+				message.channel.send('CRITICAL SUCCESS! - you rolled a nat 20!');
+			}
+>>>>>>> parent of d8345e2... Adding (dis)advantage capability, and more.
+			else{
+				message.channel.send('You rolled a ' + roll + '.');
 			}
 		}
 		else
