c4baf87724ebd37fd06600a9c4376c966701130f
nickmachiavelli7 <41245311+nickmachiavelli7@users.noreply.github.com>
2020-04-18 12:43:44 -0500

    Create 5elookup.js

---
+++ commands/5elookup.js
@@ -0,0 +1,22 @@
+module.exports = {
+	name: '5elookup',
+	description: 'Looks up a value in the D&D 5e database. Specify "spell", "item", "monster", or "ability". If spaces are needed, use dashes instead. Example command would be >5elookup ability lay-on-hands.',
+	args: true,
+	execute(message, args) {
+		var url = 'http://www.dnd5eapi.co/api/';
+		if (args[0] == 'spell')
+		{
+			https.get(url, res => {
+			res.setEncoding("utf8");
+			let body = "";
+			res.on("data", data => {
+			body += data;
+			});
+			res.on("end", () => {
+			body = JSON.parse(body);
+			console.log(body);
+			});
+			});
+		}
+		
+};
