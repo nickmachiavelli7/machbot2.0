b6c63ee6f60c05ef8dbdc5a7be47d8976331fabf
nickmachiavelli7 <41245311+vianilambo@users.noreply.github.com>
2021-11-01 18:21:04 -0500

    Update vping.js

--- commands/vping.js
+++ commands/vping.js
@@ -2,11 +2,9 @@ module.exports = {
 	name: 'vping',
 	description: 'Testing voice channel connection.',
 	execute(message) {
-		// Join the same voice channel of the author of the message
   if (message.member.voice.channel) {
-    const connection = await message.member.voice.channel.join();
-    if(message.content === 'ping'){
-      const dispatcher = connection.play('https://cdn.glitch.com/60a14f49-0846-4bad-b84a-e5f018c2130d%2Fmsn_alert.mp3?1506640405402');
+    const connection = await message.member.voice.channel.join();	
+	const dispatcher = connection.play('https://cdn.glitch.com/60a14f49-0846-4bad-b84a-e5f018c2130d%2Fmsn_alert.mp3?1506640405402');
 //connection.play('https://cdn.glitch.com/60a14f49-0846-4bad-b84a-e5f018c2130d%2Fmsn_alert.mp3?1506640405402', { volume: 0.1 });
       dispatcher.on('start', () => {
         console.log('audio is now playing!');
@@ -14,11 +12,5 @@ module.exports = {
       .catch(err => {
         console.log('error:', err);
       });
-      // Always remember to handle errors appropriately!
-      //dispatcher.destroy();
-      //Leave voice channel
-      //connection.disconnect();
-    }
-  }
-	},
+  };
 };
